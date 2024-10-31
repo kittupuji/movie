cab5d07992b925485505d514f335605335109d7f
Kirti Lulle <kirtilulle@gmail.com>
2024-10-31 21:15:58 +0530

    Update API calls to use HTTPS and add environment variable for API key

--- src/useMovies.js
+++ src/useMovies.js
@@ -1,6 +1,67 @@
+// import { useState, useEffect } from "react";
+
+// const KEY = "f84fc31d";
+
+// export function useMovies(query) {
+//   const [movies, setMovies] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState("");
+
+//   useEffect(
+//     function () {
+//       const controller = new AbortController();
+
+//       async function fetchMovies() {
+//         try {
+//           setIsLoading(true);
+//           setError("");
+
+//           const res = await fetch(
+//             `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+//             {
+//               signal: controller.signal,
+//             }
+//           );
+
+//           if (!res.ok)
+//             throw new Error("Something went wrong with fetching movies");
+
+//           const data = await res.json();
+//           if (data.Response === "False") throw new Error("Movie not found");
+
+//           setMovies(data.Search);
+//           setError("");
+//         } catch (err) {
+//           if (err.name !== "AbortError") {
+//             console.log(err.message);
+//             setError(err.message);
+//           }
+//         } finally {
+//           setIsLoading(false);
+//         }
+//       }
+
+//       if (query.length < 3) {
+//         setMovies([]);
+//         setError("");
+//         return;
+//       }
+
+//       fetchMovies();
+
+//       return function () {
+//         controller.abort();
+//       };
+//     },
+//     [query]
+//   );
+
+//   return { movies, isLoading, error };
+// }
+
 import { useState, useEffect } from "react";
 
-const KEY = "f84fc31d";
+const KEY = process.env.OMDB_API_KEY;
 
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
@@ -17,8 +78,10 @@ export function useMovies(query) {
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            {
+              signal: controller.signal,
+            }
           );
 
           if (!res.ok)
